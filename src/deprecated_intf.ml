6c0886af89db60539d2c716697068e4c8bbabab2
Xavier Clerc <xclerc@janestreet.com>
2018-06-07 11:39:27 +0100

    v0.11.117.10+09

--- src/deprecated_intf.ml
+++ src/deprecated_intf.ml
@@ -1,3 +1,18 @@
+(* New code looking for a CSV parser should use [Delimited_parsing.Csv]. It is faster,
+   tested, and supported.
+
+   For legacy code already using the deprecated interface, we have provided a drop-in
+   replacement which implements the old [Csv] interface using the new CSV parser. To
+   upgrade, make this one-line change: {[
+
+     open Delimited_parsing.Replace_deprecated_csv
+
+   ]}
+
+   Note that only the CSV parser is deprecated. The [Character_separated_without_quoting]
+   and [Positional] parsers are identical between the current and deprecated interfaces.
+*)
+
 module type Deprecated = sig
 
   module Header = Header
