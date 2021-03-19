ed30030c85548e54307c148998cfc506027a24bd
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-03-19 19:57:52 +0000

    v0.15~preview.124.21+407

--- src/read.ml
+++ src/read.ml
@@ -50,7 +50,7 @@ module Streaming = struct
   ;;
 end
 
-let fold_reader'
+let fold_reader_internal
       ?strip
       ?(skip_lines = 0)
       ?sep
@@ -60,13 +60,14 @@ let fold_reader'
       builder
       ~init
       ~f
+      ~what_to_enqueue
       r
   =
   let%bind () = Shared.drop_lines r skip_lines in
   let buffer = Bytes.create buffer_size in
   let queue = Queue.create () in
   let state =
-    Streaming.create
+    Streaming.create_indexed
       ?strip
       ?sep
       ?quote
@@ -75,7 +76,8 @@ let fold_reader'
       ?on_invalid_row
       builder
       ~init:()
-      ~f:(fun () elt -> Queue.enqueue queue elt)
+      ~f:(fun line_number () elt ->
+        Queue.enqueue queue (what_to_enqueue elt ~line_number))
   in
   Deferred.repeat_until_finished (state, init) (fun (state, acc) ->
     match%bind Reader.read r buffer ~len:buffer_size with
@@ -92,10 +94,71 @@ let fold_reader'
       `Repeat (state, acc))
 ;;
 
-let bind_without_unnecessary_yielding x ~f =
-  match Deferred.peek x with
-  | Some x -> f x
-  | None -> Deferred.bind x ~f
+let fold_readeri'
+      ?strip
+      ?skip_lines
+      ?sep
+      ?quote
+      ?header
+      ?on_invalid_row
+      builder
+      ~init
+      ~f
+      r
+  =
+  fold_reader_internal
+    ?strip
+    ?skip_lines
+    ?sep
+    ?quote
+    ?header
+    ?on_invalid_row
+    builder
+    ~init
+    ~f
+    ~what_to_enqueue:(fun line ~line_number -> line_number, line)
+    r
+;;
+
+let fold_reader' ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder ~init ~f r
+  =
+  fold_reader_internal
+    ?strip
+    ?skip_lines
+    ?sep
+    ?quote
+    ?header
+    ?on_invalid_row
+    builder
+    ~init
+    ~f
+    ~what_to_enqueue:(fun line ~line_number:_ -> line)
+    r
+;;
+
+let fold_queue acc queue ~f =
+  Queue.fold queue ~init:(return acc) ~f:(fun deferred_acc elt ->
+    let bind_without_unnecessary_yielding x ~f =
+      match Deferred.peek x with
+      | Some x -> f x
+      | None -> Deferred.bind x ~f
+    in
+    bind_without_unnecessary_yielding deferred_acc ~f:(fun acc -> f acc elt))
+;;
+
+let fold_readeri ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder ~init ~f r
+  =
+  fold_readeri'
+    ?strip
+    ?skip_lines
+    ?sep
+    ?quote
+    ?header
+    ?on_invalid_row
+    builder
+    ~init
+    r
+    ~f:(fold_queue ~f:(fun acc (line_number, elt) -> f line_number acc elt))
 ;;
 
 let fold_reader ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder ~init ~f r =
@@ -109,9 +172,7 @@ let fold_reader ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder ~
     builder
     ~init
     r
-    ~f:(fun acc queue ->
-      Queue.fold queue ~init:(return acc) ~f:(fun deferred_acc row ->
-        bind_without_unnecessary_yielding deferred_acc ~f:(fun acc -> f acc row)))
+    ~f:(fold_queue ~f)
 ;;
 
 let fold_reader_without_pushback
@@ -137,6 +198,34 @@ let fold_reader_without_pushback
     ~init
     r
     ~f:(fun acc queue -> return (Queue.fold queue ~init:acc ~f))
+;;
+
+let fold_reader_without_pushbacki
+      ?strip
+      ?skip_lines
+      ?sep
+      ?quote
+      ?header
+      ?on_invalid_row
+      builder
+      ~init
+      ~f
+      r
+  =
+  fold_readeri'
+    ?strip
+    ?skip_lines
+    ?sep
+    ?quote
+    ?header
+    ?on_invalid_row
+    builder
+    ~init
+    r
+    ~f:(fun acc queue ->
+      return
+        (Queue.fold queue ~init:acc ~f:(fun acc (line_number, elt) ->
+           f line_number acc elt)))
 ;;
 
 let pipe_of_reader ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder reader =
@@ -169,4 +258,133 @@ let pipe_of_reader ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builde
 let create_reader ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder filename =
   Reader.open_file filename
   >>| pipe_of_reader ?strip ?skip_lines ?sep ?quote ?header ?on_invalid_row builder
+;;
+
+let rec do_line_skip ~skip_lines ~skipped_so_far ~stream ~input_pipe =
+  if skipped_so_far >= skip_lines
+  then return stream
+  else (
+    match%bind Pipe.read input_pipe with
+    | `Eof ->
+      raise_s
+        [%message
+          "Delimited.Read.pipe_of_chunks: Reached EOF while skipping lines"
+            (skip_lines : int)
+            (skipped_so_far : int)]
+    | `Ok input ->
+      let rec skip_lines_in_chunk ~pos ~skipped_so_far =
+        match String.index_from input pos '\n' with
+        | None -> do_line_skip ~skip_lines ~skipped_so_far ~stream ~input_pipe
+        | Some end_pos ->
+          let skipped_so_far = skipped_so_far + 1 in
+          if skipped_so_far >= skip_lines
+          then (
+            let stream = Streaming.input_string stream ~pos:(end_pos + 1) input in
+            return stream)
+          else skip_lines_in_chunk ~pos:(end_pos + 1) ~skipped_so_far
+      in
+      skip_lines_in_chunk ~pos:0 ~skipped_so_far)
+;;
+
+let parse_pipe
+      ~newlines_between_reads
+      ?strip
+      ?(start_line_number = 1)
+      ?(skip_lines = 0)
+      ?sep
+      ?quote
+      ?header
+      ?on_invalid_row
+      builder
+      input_pipe
+  =
+  Pipe.create_reader ~close_on_exception:false (fun writer ->
+    let init =
+      Streaming.create
+        ?header
+        ?strip
+        ?sep
+        ?quote
+        ~start_line_number:(start_line_number + skip_lines)
+        ?on_invalid_row
+        builder
+        (* The accumulator is a deferred that waits for all the outstanding row outputs to
+           be consumed. Then you can get good pushback behaviour by waiting for the
+           accumulator before feeding any more input in (which indeed is what the
+           [Pipe.fold] below does). *)
+        ~init:(return ())
+        ~f:(fun _ row ->
+          (* Waiting for the accumulator is not necessary, since [Pipe.write] should not
+             become determined until all previous writes have become determined anyway. *)
+          Pipe.write writer row)
+    in
+    let%bind init =
+      do_line_skip ~skip_lines ~skipped_so_far:0 ~stream:init ~input_pipe
+    in
+    let%bind stream =
+      Pipe.fold input_pipe ~init ~f:(fun stream input ->
+        let stream = Streaming.input_string stream input in
+        let stream =
+          if newlines_between_reads
+          then Streaming.input_string stream "\n"
+          else stream
+        in
+        let%map () = Streaming.acc stream in
+        stream)
+    in
+    let stream = Streaming.finish stream in
+    (* I think this last wait for the accumulator before closing the pipe is probably
+       unnecessary. The [Streaming.finish] call should already have written everything to
+       the pipe. But it's easy to write [f] in the streaming fold such that that's not
+       true, or at least not obviously true, e.g. by waiting for the accumulator before
+       doing the pipe write. So this seems safer against possible future refactors. *)
+    Streaming.acc stream)
+;;
+
+let pipe_of_chunks
+      ?strip
+      ?start_line_number
+      ?skip_lines
+      ?sep
+      ?quote
+      ?header
+      ?on_invalid_row
+      builder
+      input_pipe
+  =
+  parse_pipe
+    ~newlines_between_reads:false
+    ?strip
+    ?start_line_number
+    ?skip_lines
+    ?sep
+    ?quote
+    ?header
+    ?on_invalid_row
+    builder
+    input_pipe
+;;
+
+let pipe_of_lines
+      ?strip
+      ?start_line_number
+      ?skip_lines
+      ?sep
+      ?quote
+      ?header
+      ?on_invalid_row
+      builder
+      input_pipe
+  =
+  parse_pipe
+    ~newlines_between_reads:true
+    ?strip
+    ?start_line_number
+    ?skip_lines
+    ?sep
+    ?quote
+    ?header
+    ?on_invalid_row
+    builder
+    input_pipe
 ;;
--- src/read.mli
+++ src/read.mli
@@ -40,7 +40,8 @@ end
     If [skip_lines] > 0, that many lines are skipped at the start of the input.
     Note that this skips lines without doing any CSV parsing of the lines being skipped,
     so newlines within a quoted field are treated identically to newlines outside a
-    quoted field.
+    quoted field. An exception will be raised if the input has fewer than [skip_lines]
+    lines.
     Default value is 0.
 
     [sep] is the character that separates fields within a row.
@@ -65,6 +66,22 @@ val fold_reader
   -> Reader.t
   -> 'b Deferred.t
 
+(** Same as [fold_reader], except that it also passes the line number of the current row
+    to [f].
+*)
+val fold_readeri
+  :  ?strip:bool
+  -> ?skip_lines:int
+  -> ?sep:char
+  -> ?quote:[ `No_quoting | `Using of char ]
+  -> ?header:Header.t
+  -> ?on_invalid_row:'a On_invalid_row.t
+  -> 'a t
+  -> init:'b
+  -> f:(int -> 'b -> 'a -> 'b Deferred.t)
+  -> Reader.t
+  -> 'b Deferred.t
+
 (** [fold_reader' ?strip ?skip_lines ?sep ?quote ~init ~f r] works similarly to
     [fold_reader], except for the [f] argument. [fold_reader'] runs [f] on batches
     of [Row.t]s rather than running [f] on each individual row.
@@ -82,6 +99,20 @@ val fold_reader'
   -> Reader.t
   -> 'b Deferred.t
 
+(** Same as [fold_reader'], except that each element in a batch of [Row.t]'s is a tuple of line number and row. *)
+val fold_readeri'
+  :  ?strip:bool
+  -> ?skip_lines:int
+  -> ?sep:char
+  -> ?quote:[ `No_quoting | `Using of char ]
+  -> ?header:Header.t
+  -> ?on_invalid_row:'a On_invalid_row.t
+  -> 'a t
+  -> init:'b
+  -> f:('b -> (int * 'a) Queue.t -> 'b Deferred.t)
+  -> Reader.t
+  -> 'b Deferred.t
+
 (** Same as [fold_reader] but the fold function does not exert pushback on the fold. *)
 val fold_reader_without_pushback
   :  ?strip:bool
@@ -96,6 +127,21 @@ val fold_reader_without_pushback
   -> Reader.t
   -> 'b Deferred.t
 
+(** Same as [fold_reader_without_pushback], except that it also passes the line number of
+    the current row to [f]. *)
+val fold_reader_without_pushbacki
+  :  ?strip:bool
+  -> ?skip_lines:int
+  -> ?sep:char
+  -> ?quote:[ `No_quoting | `Using of char ]
+  -> ?header:Header.t
+  -> ?on_invalid_row:'a On_invalid_row.t
+  -> 'a t
+  -> init:'b
+  -> f:(int -> 'b -> 'a -> 'b)
+  -> Reader.t
+  -> 'b Deferred.t
+
 (** [pipe_of_reader t reader] produces a pipe reader of parsed values. *)
 val pipe_of_reader
   :  ?strip:bool
@@ -106,6 +152,51 @@ val pipe_of_reader
   -> ?on_invalid_row:'a On_invalid_row.t
   -> 'a t
   -> Reader.t
+  -> 'a Pipe.Reader.t
+
+(** [pipe_of_chunks] is like [pipe_of_reader] except taking a pipe of input strings
+    instead of a reader.
+
+    Note that the fragmentation of the strings in the input pipe is irrelevant: they
+    aren't assumed to be separate lines, so each string can contain multiple lines or be a
+    fragment of a line, and must contain explicit newline charcters to separate each line.
+    If you're using the output of [Reader.lines] here, note that it removes the newlines,
+    so you'd need to add them back in, or see [pipe_of_lines].
+
+    [skip_lines] has the same caveat as for [fold_reader]: the lines are skipped before
+    CSV parsing starts, so it does not treat newlines within quoted strings specially, and
+    it raises an exception if the input doesn't contain at least that many lines. *)
+val pipe_of_chunks
+  :  ?strip:bool
+  -> ?start_line_number:int
+  -> ?skip_lines:int
+  -> ?sep:char
+  -> ?quote:[ `No_quoting | `Using of char ]
+  -> ?header:Header.t
+  -> ?on_invalid_row:'a On_invalid_row.t
+  -> 'a t
+  -> string Pipe.Reader.t
+  -> 'a Pipe.Reader.t
+
+(** [pipe_of_lines] is like [pipe_of_chunks], but it assumes that each element of the
+    input pipe is a separate line of a csv, inserting newlines in between them. The
+    newline insertion is not "smart" in any way, so in particular:
+
+    - input elements with unquoted newlines in them are treated as multiple lines,
+    - any existing trailing newlines in the input elements would still have a newline
+      inserted after them, resulting in blank lines,
+    - input elements that start but don't close a quoted string will lead to weird
+      results. *)
+val pipe_of_lines
+  :  ?strip:bool
+  -> ?start_line_number:int
+  -> ?skip_lines:int
+  -> ?sep:char
+  -> ?quote:[ `No_quoting | `Using of char ]
+  -> ?header:Header.t
+  -> ?on_invalid_row:'a On_invalid_row.t
+  -> 'a t
+  -> string Pipe.Reader.t
   -> 'a Pipe.Reader.t
 
 (** [create_reader filename] opens a reader for the given filename & returns a pipe of its parsed values. *)
